2658096c432aa2b6b4cef5ecfcfcdbb6434f3606
Manikandan <62287301+Manipandian@users.noreply.github.com>
2020-06-30 20:00:24 +0530

    Updates

---
+++ precache-manifest.d3eaec6a3c5553d993fe8ed618e5bc33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e821ea19440758fe2cb656d0d42f62",
+    "url": "/number-facts/index.html"
+  },
+  {
+    "revision": "a4599458b39adfddf20b",
+    "url": "/number-facts/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "87e04561e64c0381d286",
+    "url": "/number-facts/static/css/main.96a5a74d.chunk.css"
+  },
+  {
+    "revision": "a4599458b39adfddf20b",
+    "url": "/number-facts/static/js/2.fe778a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/number-facts/static/js/2.fe778a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e04561e64c0381d286",
+    "url": "/number-facts/static/js/main.7c319ef1.chunk.js"
+  },
+  {
+    "revision": "a0f975d6ea455896e330",
+    "url": "/number-facts/static/js/runtime-main.763f60eb.js"
+  }
+]);
